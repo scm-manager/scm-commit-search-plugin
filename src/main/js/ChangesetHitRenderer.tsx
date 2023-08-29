bd1a188ca33ff8b0dd16020cdca115fccb0f211b
Laura Gorzitze <laura.gorzitze@cloudogu.com>
2023-08-29 14:24:19 +0200

    Make commit search accessible
    Co-authored-by: Florian Scholdei <florian.scholdei@cloudogu.com>
    Co-authored-by: René Pfeuffer <rene.pfeuffer@cloudogu.com>

--- src/main/js/ChangesetHitRenderer.tsx
+++ src/main/js/ChangesetHitRenderer.tsx
@@ -23,14 +23,16 @@
  */
 
 import React, { FC } from "react";
-import { Hit, Notification, RepositoryAvatar, TextHitField, useStringHitFieldValue } from "@scm-manager/ui-components";
+import { Notification, RepositoryAvatar, TextHitField, useStringHitFieldValue } from "@scm-manager/ui-components";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import { useTranslation } from "react-i18next";
+import { Trans } from "react-i18next";
 import { extensionPoints } from "@scm-manager/ui-extensions";
+import { CardList } from "@scm-manager/ui-layout";
+import classNames from "classnames";
 
-const Container = styled(Hit.Content)`
-  overflow-x: auto;
+const StyledLink = styled(Link)`
+  gap: 0.5rem;
 `;
 
 const FileContent = styled.div`
@@ -40,48 +42,48 @@ const FileContent = styled.div`
 
 const ChangesetHitRenderer: FC<extensionPoints.SearchHitRenderer["props"]> = ({ hit }) => {
   const commitId = useStringHitFieldValue(hit, "id");
-  const [t] = useTranslation("plugins");
 
   const repository = hit._embedded?.repository;
+  const title = `${repository?.namespace}/${repository?.name}`;
 
   if (!commitId || !repository) {
     return <Notification type="danger">Found incomplete commit search result</Notification>;
   }
 
   return (
-    <Hit>
-      <Container className={`${repository.namespace}/${repository.name}`}>
-        <div className="is-flex">
-          <RepositoryAvatar repository={repository} size={48} />
-          <div className="ml-2">
-            <Link to={`/repo/${repository.namespace}/${repository.name}`}>
-              <Hit.Title>
-                {repository.namespace}/{repository.name}
-              </Hit.Title>
-            </Link>
-          </div>
-        </div>
-        <FileContent className="my-2">
-          <pre>
-            <code>
-              <TextHitField hit={hit} field="description" truncateValueAt={1024} />
-            </code>
-          </pre>
-        </FileContent>
-        <div className="is-flex is-justify-content-space-between is-size-7">
-          <small className="is-size-7">
-            Revision:{" "}
+      <CardList.Card key={title} className={`${repository.namespace}/${repository.name}`}>
+        <CardList.Card.Row>
+          <CardList.Card.Title>
+            <StyledLink
+              to={`/repo/${repository.namespace}/${repository.name}`}
+              className={classNames("is-flex", "is-justify-content-flex-start", "is-align-items-center")}
+            >
+              <RepositoryAvatar repository={repository} size={16} /> {title}
+            </StyledLink>
+          </CardList.Card.Title>
+        </CardList.Card.Row>
+        <CardList.Card.Row>
+          <FileContent className="my-2">
+            <pre>
+              <code>
+                <TextHitField hit={hit} field="description" truncateValueAt={1024} />
+              </code>
+            </pre>
+          </FileContent>
+        </CardList.Card.Row>
+        <CardList.Card.Row className="is-size-7 has-text-secondary">
+          <Trans i18nKey={"scm-commit-search-plugin.commit-details"}>
+            Revision{" "}
             <Link
-              className="is-ellipsis-overflow"
+              className="is-ellipsis-overflow is-relative"
               to={`/repo/${repository.namespace}/${repository.name}/code/changeset/${commitId}`}
             >
               <TextHitField hit={hit} field="id" />
-            </Link>
-          </small>
-          {t("scm-commit-search-plugin.author")}: <TextHitField hit={hit} field="author" />
-        </div>
-      </Container>
-    </Hit>
+            </Link>{" "}
+            by <TextHitField hit={hit} field="author" />
+          </Trans>
+        </CardList.Card.Row>
+      </CardList.Card>
   );
 };
 
